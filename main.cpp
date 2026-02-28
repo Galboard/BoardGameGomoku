b61e7d19060067559ef64747c8195fdbe6fe11d3
opra <g2714472748@gmail.com>
2026-02-28 22:42:43 +0800

    Code build bash

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 int main() {
     try {
         lcd screen(DEFAULT_DEVPATH);
+        screen.render_rectangle(300, 200, 100, 100, 0x00ffff00);
+        screen.show();
     } catch (std::exception& e) {
         std::cerr << e.what() << std::endl;
     }
